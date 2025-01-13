2a35c25920a508126cd38b04766e741d4575d427
Agostinho Ferreira <agostinho008.af45@gmail.com>
2025-01-13 11:18:01 -0300

    .

---
+++ exec_random_list.py
@@ -0,0 +1,15 @@
+import json
+import random
+import re
+x = "C:\\Users\\agost\\OneDrive\\Documentos\\Obsidian Vault\\Filmes - insta_1.md"
+lista_filmes = []
+with open(x, "r", encoding="utf-8") as line:
+    string = line.readlines()
+    string = string[:]
+    for i in string:
+        # regex = re.sub(r'^[0-9]{1,3}.', '',i).strip()
+        if re.findall(r'\[ \]', i) == ['[ ]']:
+            lista_filmes.append(i[:-1])
+        # print(regex)
+    # print(string)
+print(f"Filme da vez: {random.choice(lista_filmes)}")
