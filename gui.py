b771e667f596bf30ec941550ca01a13d42f4130e
Agostinho Ferreira <agostinho008.af45@gmail.com>
2025-01-13 12:05:19 -0300

    create layout

--- gui.py
+++ gui.py
@@ -2,14 +2,34 @@ import sys
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-def window():
-   app = QApplication(sys.argv)
-   w = QWidget()
-   b = QLabel(w)
-   b.setText("Hello World!")
-   b.move(50,20)
-   w.setWindowTitle("PyQt5")
-   w.show()
-   sys.exit(app.exec_())
-if __name__ == '__main__':
-   window()
+import exec_random_list
+class MainWindow(QMainWindow):
+   def __init__(self):
+      super().__init__()
+      self.setWindowTitle("My App")
+
+      # Widget central para usar layouts
+      central_widget = QWidget(self)
+      self.setCentralWidget(central_widget)
+
+      # Layout
+      layout = QGridLayout()
+      central_widget.setLayout(layout)
+
+      # Botão
+      button = QPushButton("Aperte-me")
+      button.setCheckable(True)
+      button.clicked.connect(self.get_filme)
+      button.setFixedSize(200, 80)
+
+
+      # Adiciona o botão ao layout
+      layout.addWidget(button, 0, 0)
+   def get_filme(self):
+      exec_random_list._get_filme()
+app= QApplication(sys.argv)
+
+windows = MainWindow()
+windows.resize(775, 340)
+windows.show()
+app.exec()
